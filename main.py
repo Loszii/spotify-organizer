882336aaf7a828e26a0d32789a8a8bd5a7adcedf
Loszi <jiozzi11@gmail.com>
2023-11-28 22:51:49 -0500

    removed hours from date and fixed sorting of secondary value

--- main.py
+++ main.py
@@ -15,13 +15,7 @@ def minConvert():
     for i in allSongs:
         allSongs[i][0] = round(allSongs[i][0] / 60000, 1)
 
-def getSongDate(i):
-    if allSongs[i["trackName"], i["artistName"]][1] == "":
-        return i["endTime"]
-    else:
-        return allSongs[i["trackName"], i["artistName"]][1]
-
-def getArtistDate(current, prev):
+def getDate(current, prev):
     if prev == "":
         return current
     else:
@@ -36,7 +30,7 @@ def initialize(data):
 def organize(data):
     for j in data:
         for i in j:
-            allSongs[(i["trackName"], i["artistName"])] = [allSongs[(i["trackName"], i["artistName"])][0] + i["msPlayed"], getSongDate(i)]
+            allSongs[(i["trackName"], i["artistName"])] = [allSongs[(i["trackName"], i["artistName"])][0] + i["msPlayed"], getDate(i["endTime"], allSongs[(i["trackName"], i["artistName"])][1])]
 
 def getTotalMin():
     count = 0
@@ -93,12 +87,12 @@ def sortByArtist(config):
     for i in allSongs:
         dataPerArtist[i[1]] = [0, ""]
     for i in allSongs:
-        dataPerArtist[i[1]] = [dataPerArtist[i[1]][0] + allSongs[i][0], getArtistDate(allSongs[i][1], dataPerArtist[i[1]][1])]
+        dataPerArtist[i[1]] = [dataPerArtist[i[1]][0] + allSongs[i][0], getDate(allSongs[i][1], dataPerArtist[i[1]][1])]
     if config == "d":
         for i in dataPerArtist:
-            if dataPerArtist[i][0] >= mins:
-                final.append((dataPerArtist[i][1], i, round(dataPerArtist[i][0], 1), round(dataPerArtist[i][0] / 60, 1))) #need to round again since floating points
-                #final = [(date, artist, min, hour)...]
+            if dataPerArtist[i][0] >= mins: 
+                final.append((dataPerArtist[i][1].split(" ")[0], round(dataPerArtist[i][0], 1), i, round(dataPerArtist[i][0] / 60, 1))) #need to round again since floating points
+                #final = [(date, min, artist, hour)...]
         allArtists = getTotalArtist()
         final.sort()
         txt = open("output.txt", "a", encoding="utf-8")
@@ -106,14 +100,14 @@ def sortByArtist(config):
         txt.write(f"\nAll artists listened to for more than {mins} minutes:\n\n")
         txt.write(f"First Listened | Artist | Minutes | Hours")
         for i in final:
-            txt.write(f"\n{i[0]} | {i[1]} | {i[2]} | {i[3]}")
+            txt.write(f"\n{i[0]} | {i[2]} | {i[1]} | {i[3]}")
         txt.write(f"\n\nThe total number of artists listened to for more than {mins} minutes is {len(final)}")
         txt.write(f"\nThe total number of artists within all data is {allArtists}")
     else:
         for i in dataPerArtist:
             if dataPerArtist[i][0] >= mins: # 
-                final.append((round(dataPerArtist[i][0], 1), i, dataPerArtist[i][1], round(dataPerArtist[i][0] / 60, 1))) #need to round again since floating points
-                #final = [(min, artist, date, hour)...]
+                final.append((round(dataPerArtist[i][0], 1), dataPerArtist[i][1].split(" ")[0], i, round(dataPerArtist[i][0] / 60, 1))) #need to round again since floating points
+                #final = [(min, date, artist, hour)...]
         allArtists = getTotalArtist()
         final.sort()
         txt = open("output.txt", "a", encoding="utf-8")
@@ -121,7 +115,7 @@ def sortByArtist(config):
         txt.write(f"\nAll artists listened to for more than {mins} minutes:\n\n")
         txt.write(f"First Listened | Artist | Minutes | Hours")
         for i in final:
-            txt.write(f"\n{i[2]} | {i[1]} | {i[0]} | {i[3]}")
+            txt.write(f"\n{i[1]} | {i[2]} | {i[0]} | {i[3]}")
         txt.write(f"\n\nThe total number of artists listened to for more than {mins} minutes is {len(final)}")
         txt.write(f"\nThe total number of artists within all data is {allArtists}")
     txt.close()
