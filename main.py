b38bea947057d231f53879911a573a2675da722c
Loszi <jiozzi11@gmail.com>
2023-11-28 22:40:32 -0500

    Added sorting by min or date option, now prints both songs nad artists

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 
 files = []
 allSongs = {}
+mins = int(input("Enter minimum number of minutes listened (0 to see all data): "))
+#add when first listened to artist
+
 
 for i in os.listdir(os.getcwd()):
     if "StreamingHistory" in i:
@@ -12,11 +15,17 @@ def minConvert():
     for i in allSongs:
         allSongs[i][0] = round(allSongs[i][0] / 60000, 1)
 
-def getDate(i):
+def getSongDate(i):
     if allSongs[i["trackName"], i["artistName"]][1] == "":
         return i["endTime"]
     else:
         return allSongs[i["trackName"], i["artistName"]][1]
+
+def getArtistDate(current, prev):
+    if prev == "":
+        return current
+    else:
+        return prev
 
 def initialize(data):
     for j in data:
@@ -27,7 +36,7 @@ def initialize(data):
 def organize(data):
     for j in data:
         for i in j:
-            allSongs[(i["trackName"], i["artistName"])] = [allSongs[(i["trackName"], i["artistName"])][0] + i["msPlayed"], getDate(i)]
+            allSongs[(i["trackName"], i["artistName"])] = [allSongs[(i["trackName"], i["artistName"])][0] + i["msPlayed"], getSongDate(i)]
 
 def getTotalMin():
     count = 0
@@ -44,46 +53,77 @@ def getTotalArtist():
             memory[i[1]] = True
     return count
         
-
-def sortByTotal():
+def sortBySong(config):
     final = []
     allMinutes = round(getTotalMin())
-    mins = int(input("Enter minimum number of minutes listened (0 to see all data): "))
-    for i in allSongs:
-        if allSongs[i][0] >= mins:
-            final.append((allSongs[i][1].split(" ")[0], allSongs[i][0], i[0], i[1]))
-            #final = [(date, time, title, artist), ...]
-    final.sort()
-    txt = open("output.txt", "w", encoding="utf-8")
-    txt.write(f"All Songs listened to for more than {mins} minutes:\n\nFirst Listened | Minutes | Title | Artist")
-    txt.close()
-    txt = open("output.txt", "a", encoding="utf-8")
-    for i in final:
-        txt.write(f"\n{i[0]} | {i[1]} | {i[2]} | {i[3]}")
-    txt.write(f"\n\nThe total number of songs listened to for more than {mins} minutes is {len(final)}")
-    txt.write(f"\nThe total number of minutes within all data is {allMinutes} minutes, which is {round(allMinutes / 24)} days")
+    if config == "d":
+        for i in allSongs:
+            if allSongs[i][0] >= mins:
+                final.append((allSongs[i][1].split(" ")[0], allSongs[i][0], i[0], i[1]))
+                #final = [(date, time, title, artist), ...]
+        final.sort()
+        txt = open("output.txt", "w", encoding="utf-8")
+        txt.write(f"SORTED BY DATE\nAll Songs listened to for more than {mins} minutes:\n\nFirst Listened | Minutes | Title | Artist")
+        txt.close()
+        txt = open("output.txt", "a", encoding="utf-8")
+        for i in final:
+            txt.write(f"\n{i[0]} | {i[1]} | {i[2]} | {i[3]}")
+        txt.write(f"\n\nThe total number of songs listened to for more than {mins} minutes is {len(final)}")
+        txt.write(f"\nThe total number of minutes within all data is {allMinutes} minutes, which is {round(allMinutes / 24)} days\n\n")
+    else:
+        for i in allSongs:
+            if allSongs[i][0] >= mins:
+                final.append((allSongs[i][0], allSongs[i][1].split(" ")[0], i[0], i[1]))
+                #final = [(time, date, title, artist), ...]
+        final.sort()
+        txt = open("output.txt", "w", encoding="utf-8")
+        txt.write(f"SORTED BY MIN\nAll Songs listened to for more than {mins} minutes:\n\nFirst Listened | Minutes | Title | Artist")
+        txt.close()
+        txt = open("output.txt", "a", encoding="utf-8")
+        for i in final:
+            txt.write(f"\n{i[1]} | {i[0]} | {i[2]} | {i[3]}")
+        txt.write(f"\n\nThe total number of songs listened to for more than {mins} minutes is {len(final)}")
+        txt.write(f"\nThe total number of minutes within all data is {allMinutes} minutes, which is {round(allMinutes / 24)} days\n\n")
+
     txt.close()
 
-def sortByArtist():
-    minPerArtist = {} #artist: time
+def sortByArtist(config):
     final = []
-    mins = int(input("Enter minimum number of minutes listened (0 to see all data): "))
+    dataPerArtist = {} #artist: (date, time)
     for i in allSongs:
-        minPerArtist[i[1]] = 0
+        dataPerArtist[i[1]] = [0, ""]
     for i in allSongs:
-        minPerArtist[i[1]] += allSongs[i][0]
-    for i in minPerArtist:
-        if minPerArtist[i] >= mins:
-            final.append((round(minPerArtist[i], 1), round(minPerArtist[i] / 60, 1), i)) #need to round again since floating points
-            #final = [(min, hour, artist)...]
-    allArtists = getTotalArtist()
-    final.sort()
-    txt = open("output.txt", "w", encoding="utf-8")
-    txt.write(f"All artists listened to for more than {mins} minutes:\n\nArtist | Minutes | Hours")
-    for i in final:
-        txt.write(f"\n{i[2]} | {i[0]} | {i[1]}")
-    txt.write(f"\n\nThe total number of artists listened to for more than {mins} minutes is {len(final)}")
-    txt.write(f"\nThe total number of artists within all data is {allArtists}")
+        dataPerArtist[i[1]] = [dataPerArtist[i[1]][0] + allSongs[i][0], getArtistDate(allSongs[i][1], dataPerArtist[i[1]][1])]
+    if config == "d":
+        for i in dataPerArtist:
+            if dataPerArtist[i][0] >= mins:
+                final.append((dataPerArtist[i][1], i, round(dataPerArtist[i][0], 1), round(dataPerArtist[i][0] / 60, 1))) #need to round again since floating points
+                #final = [(date, artist, min, hour)...]
+        allArtists = getTotalArtist()
+        final.sort()
+        txt = open("output.txt", "a", encoding="utf-8")
+        txt.write("-------------------------------------------------\n" * 5)
+        txt.write(f"\nAll artists listened to for more than {mins} minutes:\n\n")
+        txt.write(f"First Listened | Artist | Minutes | Hours")
+        for i in final:
+            txt.write(f"\n{i[0]} | {i[1]} | {i[2]} | {i[3]}")
+        txt.write(f"\n\nThe total number of artists listened to for more than {mins} minutes is {len(final)}")
+        txt.write(f"\nThe total number of artists within all data is {allArtists}")
+    else:
+        for i in dataPerArtist:
+            if dataPerArtist[i][0] >= mins: # 
+                final.append((round(dataPerArtist[i][0], 1), i, dataPerArtist[i][1], round(dataPerArtist[i][0] / 60, 1))) #need to round again since floating points
+                #final = [(min, artist, date, hour)...]
+        allArtists = getTotalArtist()
+        final.sort()
+        txt = open("output.txt", "a", encoding="utf-8")
+        txt.write("-------------------------------------------------\n" * 5)
+        txt.write(f"\nAll artists listened to for more than {mins} minutes:\n\n")
+        txt.write(f"First Listened | Artist | Minutes | Hours")
+        for i in final:
+            txt.write(f"\n{i[2]} | {i[1]} | {i[0]} | {i[3]}")
+        txt.write(f"\n\nThe total number of artists listened to for more than {mins} minutes is {len(final)}")
+        txt.write(f"\nThe total number of artists within all data is {allArtists}")
     txt.close()
 
 #function calls
@@ -91,16 +131,19 @@ initialize(files)
 organize(files)
 minConvert()
 
+
 def menu():
-    option = input("Enter \"t\" for total stats\nEnter \"a\" to see minutes per artists\nEnter \"w\" to whitelist artists\nEnter \"b\" to blacklist artists\nEnter anything else to quit\n")
-    if option == "t":
-        sortByTotal()
-    elif option == "a":
-        sortByArtist()
-    elif option == "w":
-        pass #implement
-    elif option == "b":
-        pass #implement
-    else:
-        exit()
+    count = 0
+    while count != 1:
+        option = input("Enter \"d\" to sort by date or \"m\" to sort by minutes\n")
+        if option == "d":
+            sortBySong(option)
+            sortByArtist(option)
+            count = 1
+        elif option == "m":
+            sortBySong(option)
+            sortByArtist(option)
+            count = 1
+        else:
+            pass
 menu()
